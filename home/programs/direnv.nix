b165fbd6de2d83ad126a42ab64fbdd8886b227e5
Dante Suarez <dante.suarezferrin1@gmail.com>
2026-02-15 13:30:06 -0300

    arreglo config direnv

--- home/programs/direnv.nix
+++ home/programs/direnv.nix
@@ -6,9 +6,15 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    
+    stdlib = ''
+      use_devenv() {
+        watch_file devenv.nix
+        watch_file devenv.yaml
+        watch_file devenv.local.nix
+        
+        eval "$(devenv shell --print-bash-setup)"
+      }
+    '';
   };
-
-  home.file.".direnvrc".text = ''
-    source ${pkgs.devenv}/share/devenv/direnvrc
-  '';
 }
