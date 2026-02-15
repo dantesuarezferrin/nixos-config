a4c3916cde35e76da94cfa3b425e527a67de3c6a
Dante Suarez <dante.suarezferrin1@gmail.com>
2026-02-15 13:24:05 -0300

    arreglo: error configuracion direnv

--- home/programs/direnv.nix
+++ home/programs/direnv.nix
@@ -1,17 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
+
 {
+  home.packages = [ pkgs.devenv ];
+
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
   };
-  home.sessionVariables = {
-    DIRENV_LOG_FORMAT = "";
-  };
-  config = {
-    bash = {
-      source = ''
-        source ${pkgs.devenv}/share/devenv/direnvrc
-      '';
-    };
-  };
+
+  home.file.".direnvrc".text = ''
+    source ${pkgs.devenv}/share/devenv/direnvrc
+  '';
 }
