adb86bae7b3c7c8067757a14d445856ae2bfd24d
Dante Suarez <dante.suarezferrin1@gmail.com>
2026-02-15 21:21:31 -0300

    Añade: Alias de bash para entrar a nixos-config con nvim y una funcion en bash para pushear a git

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -7,10 +7,20 @@
     shellAliases = {
       ll = "ls -l";
       update = "sudo nixos-rebuild switch --flake ~/nixos-config#notebook";
+      config = "cd ~/nixos-config && nvim";
     };
 
     initExtra = ''
       eval "$(direnv hook bash)"
+      update-git() {
+      if [ -z "$1" ]; then
+        echo "Error: Debes proporcionar una descripción para el commit."
+        return 1
+      fi
+      git add .
+      git commit -m "$1"
+      git push origin main
+      }
     '';
   };
 }
