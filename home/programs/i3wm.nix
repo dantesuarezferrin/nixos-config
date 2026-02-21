5f101f2704624a00630b6a3c24c5845f98d480d7
Dante Suarez <dante.suarezferrin1@gmail.com>
2026-02-21 10:26:22 -0300

    Configuracion de monitores y ventanas en i3wm

--- home/programs/i3wm.nix
+++ home/programs/i3wm.nix
@@ -5,6 +5,8 @@
 		i3status
 		dmenu
 		i3lock
+		arandr
+		feh
 	];
 
 
@@ -13,6 +15,75 @@
 		config = {
 			modifier = "Mod4";
 
+			#Startup
+			startup = [
+			
+				{
+				#Display
+				command = "xrandr --output HDMI-1 --primary --mode 1920x1080 --rate 144.00";
+				always = true;
+				notification = false;
+				};
+
+				{
+				#Wallpaper
+				command = "feh --bg-fill '/home/dante/Pictures/redflow.jpg'";
+				always = true;
+				notification = false;
+				}
+			];
+
+			# Windows 
+			window = {
+				titlebar = false;
+				border = 1;
+			};
+
+			floating = {
+				titlebar = false;
+				border = 1;
+			};
+			
+			# Colors
+			colors = let 
+        bg      = "#282828";
+        red     = "#cc241d";  
+        gray    = "#928374";
+        text    = "#ebdbb2";
+        inactive = "#3c3836";
+      in {
+        #               border  bg      text    indicator childBorder
+        focused = {
+          border = red; 
+          background = red; 
+          text = text; 
+          indicator = red; 
+          childBorder = red;
+        };
+        focusedInactive = {
+          border = inactive; 
+          background = inactive; 
+          text = gray; 
+          indicator = inactive; 
+          childBorder = inactive;
+        };
+        unfocused = {
+          border = bg; 
+          background = bg; 
+          text = gray; 
+          indicator = bg; 
+          childBorder = bg;
+        };
+        urgent = {
+          border = "#fb4934"; 
+          background = "#fb4934"; 
+          text = text; 
+          indicator = "#fb4934"; 
+          childBorder = "#fb4934";
+        };
+      };
+
+			#Keybindings
 			keybindings = let
 				modifier = "Mod4";
 			in lib.mkOptionDefault {
